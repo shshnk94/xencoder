2563d658775067aefd1e303b2f9669cbb8c30634
shshnk94 <shshnk94@gmail.com>
2020-04-22 17:14:08 -0700

    Tokenizer for Vanilla transformer

--- xencoder/data/preprocess.py
+++ xencoder/data/preprocess.py
@@ -2,11 +2,52 @@ import argparse
 import pandas as pd
 import numpy as np
 import h5py
-from transformers import XLMRobertaTokenizer
+
+def get_tokenizer(args):
+
+    if args.tokenizer == 'xlmr':
+
+        from transformers import XLMRobertaTokenizer
+        xlmrtokenizer = XLMRobertaTokenizer.from_pretrained('xlm-roberta-large')
+
+        def tokenizer(sentence):
+            return xlmrtokenizer.convert_tokens_to_ids(xlmrtokenizer.tokenize(sentence))
+        
+        return tokenizer
+
+    elif args.tokenizer == 'vanilla':
+    
+        from fairseq.data.encoders.subword_nmt_bpe import SubwordNMTBPE
+        from fairseq.data.encoders.moses_tokenizer import MosesTokenizer
+        from fairseq.data import Dictionary        
+ 
+        class Args:
+
+            def __init__(self):
+
+                self.moses_source_lang = 'en'
+                self.moses_target_lang = 'fr'
+                self.moses_no_dash_splits = False
+                self.moses_no_escape = False
+                self.bpe_codes = './bpecodes'
+                self.bpe_separator = '@@'
+
+        def tokenizer(sentence):
+
+            attrs = Args()
+            
+            tokenizer = MosesTokenizer(attrs)
+            bpe = SubwordNMTBPE(attrs)
+            dictionary = Dictionary.load('dict.en.txt')
+            
+            return dictionary.encode_line(bpe.encode(sentence))
+           
+        return tokenizer
 
 def process_data(args):
-       
-    tokenizer = XLMRobertaTokenizer.from_pretrained('xlm-roberta-large')
+    
+    tokenizer = get_tokenizer(args)   
+
     df = pd.read_csv(args.source, chunksize=args.chunksize, sep='\t', header=None)
     
     #Creating HDF5 dataset
@@ -19,7 +60,7 @@ def process_data(args):
  
         for sentence in chunk[0]:
 
-            token_ids = np.array(tokenizer.convert_tokens_to_ids(tokenizer.tokenize(sentence)))
+            token_ids = np.array(tokenizer(sentence))
             if len(token_ids) < args.max_length:
                 sentences.append(token_ids)
         
@@ -44,6 +85,7 @@ if __name__ == '__main__':
     parser.add_argument('--chunksize', type=int, help='Chunk size')
     parser.add_argument('--save_path', type=str, help='Destination to store the adapter')
     parser.add_argument('--max_length', type=int, help='Maximum length of a sequence of tokens (not words)')
+    parser.add_argument('--tokenizer', type=str, help='Tokenizer depending on XLMR vs Encoder in Vaswani et. al')
 
     args = parser.parse_args()
 
